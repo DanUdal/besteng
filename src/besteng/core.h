3e02f629c6317705cf7ec79ad2f8297966d7ef76
DanUdal <48066593+DanUdal@users.noreply.github.com>
2021-01-14 17:54:32 +0000

    Update core.h

--- src/besteng/core.h
+++ src/besteng/core.h
@@ -10,29 +10,30 @@
 #include "transform.h"
 #include "inputManager.h"
 #include "collider.h"
+#include "Renderer.h"
 
 namespace besteng
 {
 	struct Core
 	{
 	private:
-		std::vector<std::shared_ptr<Entity>> entities;
-		std::weak_ptr<Core> self;
-		std::shared_ptr<Environment> environment;
-		Core();
-		SDL_Window* window;
-		Core& operator=(const Core&);
-		std::shared_ptr<Entity> currentScreen;
-		std::shared_ptr<InputManager> Input;
-		bool GJK(std::shared_ptr<Collider> ObjectA, std::shared_ptr<Collider> ObjectB);
-		void collisionTest();
+		std::vector<std::shared_ptr<Entity>> entities; //vector of all entities in the scene
+		std::weak_ptr<Core> self; //self pointer
+		std::shared_ptr<Environment> environment; //the environment holds system variables such as delta time
+		Core(); //private constructor so the user must use initialise
+		SDL_Window* window; //window pointer for sdl
+		Core& operator=(const Core&); //this makes core uncopiable
+		std::shared_ptr<Entity> currentScreen; //holds the current main camera
+		std::shared_ptr<InputManager> Input; //the input manager contains all keyboard and button inputs
+		bool GJK(std::shared_ptr<Collider> ObjectA, std::shared_ptr<Collider> ObjectB); //this function calculates if the 2 objects collide
+		void collisionTest(); //tests for collision between all entities in the scene
 	public:
-		static std::shared_ptr<Core> initialise();
-		std::shared_ptr<Entity> addEntity(std::string name);
-		std::shared_ptr<Entity> addEntity(std::string name, std::shared_ptr<Transform> position);
-		void start();
-		SDL_GLContext glContext;
-		std::shared_ptr<rend::Context> context;
+		static std::shared_ptr<Core> initialise(); //initialises the core for the engine
+		std::shared_ptr<Entity> addEntity(std::string name); //2 overloads for add entity with one defaulting to 0 and the other taking a transform for the starting position
+		std::shared_ptr<Entity> addEntity(std::string name, std::shared_ptr<Transform> position); 
+		void start(); //runs the game's main loop
+		SDL_GLContext glContext; //contains the context for sdl
+		std::shared_ptr<rend::Context> context; //context for rend
 		std::shared_ptr<Entity> getCurrentScreen()
 		{
 			return currentScreen;

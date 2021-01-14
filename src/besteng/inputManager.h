f3a6466851bf26020d2c3211e340cecf3d9643cc
DanUdal <48066593+DanUdal@users.noreply.github.com>
2021-01-14 17:55:23 +0000

    Update inputManager.h

--- src/besteng/inputManager.h
+++ src/besteng/inputManager.h
@@ -5,7 +5,7 @@
 
 namespace besteng
 {
-	struct InputManager
+	struct InputManager //the input manager saves all inputs in vectors for the user to access
 	{
 	friend struct Core;
 	private:
@@ -14,11 +14,15 @@ namespace besteng
 		std::vector<int> keyPressed;
 		std::vector<int> mouseDown;
 		std::vector<int> mousePressed;
-		std::vector<int> mouseUp;
+		std::vector<int> mouseUp; //vectors tracking all the button presses
 		void clear();
 		void checkInput();
 	public:
 		bool getKeyboardInput(int key);
-		bool getMouseInput(int button);
+		bool findKeyUp(int key);
+		bool findKeyDown(int key);
+		bool findMouseUp(int key);
+		bool findMouseDown(int key);
+		bool getMouseInput(int button); //functions for the user to check inputs
 	};
 }

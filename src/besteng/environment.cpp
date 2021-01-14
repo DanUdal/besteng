0d2479ae5e438caef83ebbd53e5e106138efea4f
DanUdal <48066593+DanUdal@users.noreply.github.com>
2021-01-14 17:55:03 +0000

    Update environment.cpp

--- src/besteng/environment.cpp
+++ src/besteng/environment.cpp
@@ -4,15 +4,20 @@ namespace besteng
 {
 	void Environment::updateDeltaTime()
 	{
-		deltaTime = (SDL_GetTicks() - lastTime) / 1000.0f;
-		lastTime = SDL_GetTicks();
+		thisTime = (SDL_GetTicks() - lastTime) / 1000.0f;
+		lastTime = SDL_GetTicks(); //updates the time that the last frame took
     }
 
 	void Environment::frameCap()
 	{
-		if (deltaTime - (1.0f / 60.0f) < 0)
+		if (thisTime - (1.0f / 60.0f) < 0)
 		{
-			SDL_Delay((1.0f / 60.0f) - (deltaTime * 1000.0f));
+			SDL_Delay((1.0f / 60.0f) - (thisTime * 1000.0f));
+			deltaTime = 1.0f / 60.0f; //if the frame took less than 16.7 ms set delta time to 60 fps
+		}
+		else
+		{
+			deltaTime = thisTime; //if the frame took longer set delta time to that
 		}
 	}
 

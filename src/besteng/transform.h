70b572321bd106422da2ed1d9bfc4a6e7696a985
DanUdal <48066593+DanUdal@users.noreply.github.com>
2021-01-14 17:55:47 +0000

    Update transform.h

--- src/besteng/transform.h
+++ src/besteng/transform.h
@@ -4,13 +4,13 @@
 
 namespace besteng
 {
-	struct Transform : public Component
+	struct Transform : public Component 
 	{
 	private:
 
 	public:
-		rend::vec3 position;
-		rend::vec3 rotation;
+		rend::vec3 position; //the transform component contains the position rotation and scale of all entities
+		rend::vec3 rotation; //all entities must have a transform component
 		rend::vec3 scale;
 		rend::mat4 getModel()
 		{
@@ -19,7 +19,7 @@ namespace besteng
 			rend::mat4 rotationz = rend::rotate(rotationz, rotation.z, rend::vec3(0, 0, 1));
 			rend::mat4 translation = rend::translate(translation, position);
 			rend::mat4 scalar = rend::scale(scalar, scale);
-			return rotationx * rotationy * rotationz * translation * scalar;
+			return rotationx * rotationy * rotationz * translation * scalar; //returns a 4x4 matrix that transforms an from object space to world space
 		}
 	};
 }

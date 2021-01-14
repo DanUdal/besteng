76069f1f8b5a439ae7a8605cf31cdaf3c14d645a
DanUdal <48066593+DanUdal@users.noreply.github.com>
2021-01-14 17:54:57 +0000

    Update entity.h

--- src/besteng/entity.h
+++ src/besteng/entity.h
@@ -12,17 +12,17 @@ namespace besteng
 	{
 	friend struct Core;
 	private:
-		std::vector<std::shared_ptr<Component>> components;
+		std::vector<std::shared_ptr<Component>> components; //a list of all the compoents attached to the entity
 		void tick();
 		void render();
-		void collision(Collider other);
+		void collision(Collider other); //these call the corresponding functions in all the compoents for the entity
 		std::weak_ptr<Entity> self;
-		std::weak_ptr<Core> core;
+		std::weak_ptr<Core> core; //pointers to self and core
 	public:
 		template <typename T>
 		std::shared_ptr<T> addComponent()
 		{
-			std::shared_ptr<T> rtn = std::make_shared<T>();
+			std::shared_ptr<T> rtn = std::make_shared<T>(); //adds a component of the type specific with no variables set
 
 			components.push_back(rtn);
 
@@ -36,18 +36,18 @@ namespace besteng
 		template <typename T>
 		std::shared_ptr<T> addComponent(std::shared_ptr<T> component)
 		{
-			components.push_back(component);
+			components.push_back(component); //adds the compoent passed into the function allowing for intialisation
 
 			component->entity = self;
 
-			component->self = rtn;
+			component->self = rtn; //sets poitners
 
 			return rtn;
 		}
 
 		template <typename T>
-		std::shared_ptr<T> getComponent()
-		{
+		std::shared_ptr<T> getComponent() //returns the first component found of the specified type
+		{ //no entity should have more than 1 component of each type
 			std::shared_ptr<T> rtn = NULL;
 			for (ci = 0; ci < components.size(); ci++)
 			{
@@ -59,16 +59,16 @@ namespace besteng
 				}
 			}
 
-			throw Exception("Cannot find component type");
+			throw Exception("Cannot find component type"); //sends an exception if no component found
 		}
 		std::shared_ptr<Core> getCore()
 		{
 			return core.lock();
 		}
-		std::string name;
+		std::string name; //stores the name of the entity
 		std::shared_ptr<Screen> getCurrentScreen()
 		{
-			return getCore()->getCurrentScreen()->getComponent<Screen>();
+			return getCore()->getCurrentScreen()->getComponent<Screen>(); //set of pointer calls to make getting the screen easier for the user
 		}
 	};
 }
